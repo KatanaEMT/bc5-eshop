92f57ffa91aae3d9005ce942f7558de0139c71f1
Amankaium <kaium@list.ru>
2024-07-11 11:33:07 +0600

    added fks

--- core/models.py
+++ core/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(verbose_name="Описание", null=True, blank=False)    
+    
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     price = models.IntegerField()
     qty = models.IntegerField(default=0)
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        verbose_name="Категория",
+    )
+    
+    def __str__(self):
+        return self.name
         
