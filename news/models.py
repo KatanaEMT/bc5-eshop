4eabbc52514823ecbbeb7ca6d6ef50c5c4da18ea
Amankaium <kaium@list.ru>
2024-07-11 12:03:28 +0600

    add new m2m to user

--- news/models.py
+++ news/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class New(models.Model):
     title = models.CharField(max_length=50)
     article = models.TextField()
     views = models.IntegerField(default=0)
+    user_views = models.ManyToManyField(
+        to=User,
+        blank=True,
+    )
     
     def __str__(self):
         return self.title
